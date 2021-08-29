260d1a64e99016e3d4962d0eade5188038ff8a51
ehdgus5500@naver.com <>
2021-08-29 13:37:30 +0900

    [Drag Selecting Units] Drag안에 있는 유닛들 검사하기 구현 - Physics.BoxCastAll 로 Drag한 부분만큼의 크기의 Box를 투사하여 검사 - 우선은 SerializeField로 LayerMask 설정한 Collider만 필터링

--- Assets/Scripts/UI/DragReceiver.cs
+++ Assets/Scripts/UI/DragReceiver.cs
@@ -11,6 +11,8 @@ namespace RTSZombie.UI
     {
         [SerializeField] private Image dragIndicatorPrefab;
 
+        [SerializeField] private LayerMask targetLayer;
+
         private Image indicatorInstance;
 
         private Vector2 beginPosition;
@@ -39,11 +41,57 @@ namespace RTSZombie.UI
             indicatorInstance.GetComponent<RectTransform>().anchoredPosition = new Vector2(
                 Mathf.Min(beginPosition.x, draggingPosition.x), Mathf.Min(beginPosition.y, draggingPosition.y)
             );
+
+            //Ray beginPositionRay = Camera.main.ScreenPointToRay(beginPosition);
+            //Ray draggingPositionRay = Camera.main.ScreenPointToRay(draggingPosition);
+
+            //float minX = Mathf.Min(beginPosition.x, draggingPosition.x);
+            //float maxX = Mathf.Max(beginPosition.x, draggingPosition.x);
+            //float minY = Mathf.Min(beginPosition.y, draggingPosition.y);
+            //float maxY = Mathf.Max(beginPosition.y, draggingPosition.y);
+            //Ray bottomLeftRay = Camera.main.ScreenPointToRay(new Vector2(minX, minY));
+            //Ray bottomRightRay = Camera.main.ScreenPointToRay(new Vector2(maxX, minY));
+            //Ray topLeftRay = Camera.main.ScreenPointToRay(new Vector2(minX, maxY));
+            //Ray topRightRay = Camera.main.ScreenPointToRay(new Vector2(maxX, maxY));
+
+            //Debug.DrawRay(bottomLeftRay.origin, bottomLeftRay.direction, Color.red, 1f);
+            //Debug.DrawRay(bottomRightRay.origin, bottomRightRay.direction, Color.red, 1f);
+            //Debug.DrawRay(topLeftRay.origin, topLeftRay.direction, Color.red, 1f);
+            //Debug.DrawRay(topRightRay.origin, topRightRay.direction, Color.red, 1f);
         }
 
         public void OnEndDrag(PointerEventData eventData)
         {
             RZDebug.Log(this, $"OnEndDrag point: {eventData.position}");
+
+            Vector3 beginPositionInWorld = Camera.main.ScreenToWorldPoint(beginPosition);
+            Vector3 draggingPositionInWorld = Camera.main.ScreenToWorldPoint(draggingPosition);
+            float minXInWorld = Mathf.Min(beginPositionInWorld.x, draggingPositionInWorld.x);
+            float maxXInWorld = Mathf.Max(beginPositionInWorld.x, draggingPositionInWorld.x);
+            float minYInWorld = Mathf.Min(beginPositionInWorld.y, draggingPositionInWorld.y);
+            float maxYInWorld = Mathf.Max(beginPositionInWorld.y, draggingPositionInWorld.y);
+            Vector3 boxExtents = new Vector3(
+                Mathf.Abs(beginPositionInWorld.x - draggingPositionInWorld.x),
+                Mathf.Abs(beginPositionInWorld.y - draggingPositionInWorld.y),
+                1f
+            );
+
+            RaycastHit[] hits = Physics.BoxCastAll(
+                Camera.main.transform.position,
+                boxExtents,
+                Camera.main.transform.forward,
+                Quaternion.LookRotation(Camera.main.transform.forward, Camera.main.transform.up),
+                Mathf.Infinity,
+                targetLayer
+            );
+
+            if(hits.Length > 0)
+            {
+                foreach(var hit in hits)
+                {
+                    RZDebug.Log(this, $"BoxCastAll Hits: {hit.collider.gameObject.name}");
+                }
+            }
 
             Destroy(indicatorInstance.gameObject);
         }
--- Assets/Scripts/UI/MainCanvas.cs
+++ Assets/Scripts/UI/MainCanvas.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace RTSZombie.UI
 {
     public class MainCanvas : MonoBehaviour
     {
+        private Canvas canvas;
 
+        private CanvasScaler canvasScaler;
+
+        private void Start()
+        {
+            canvas = GetComponent<Canvas>();
+            Debug.Assert(canvas != null);
+
+            canvasScaler = GetComponent<CanvasScaler>();
+            Debug.Assert(canvasScaler != null);
+
+            float screenWidth = Screen.width;
+            float screenHeight = Screen.height;
+
+            canvasScaler.referenceResolution = new Vector2(screenWidth, screenHeight);
+        }
     }
 
 }
